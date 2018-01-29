ffb89544ad5c794807402c5a11b1bd0873dd8c03
estorgio <fortunato.estorgio@gmail.com>
2018-01-29 22:31:41 +0800

    YelpCamp Auth Pt. 2

--- app.js
+++ app.js
@@ -20,6 +20,20 @@ mongoose.connect('mongodb://localhost/yelp_camp', function (err) {
 
 seedDB();
 
+
+// PASSPORT CONFIGURATION
+app.use(require('express-session')({
+  secret: 'H59TG5xkicoQRr4gv9BijYz26KOKFViv7iio4OAZMlc15PHtuFj7reeYyXtVMvS8',
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'ejs');
@@ -127,6 +141,30 @@ app.post('/campgrounds/:id/comments', function (req, res) {
     }
   })
 });
+
+
+// ==================
+// AUTH ROUTES
+// ==================
+
+// Show register form
+app.get('/register', function (req, res) {
+  res.render('register');
+});
+
+app.post('/register', function (req, res) {
+  var newUser = new User({username: req.body.username});
+  User.register(newUser, req.body.password, function (err, user) {
+    if (err) {
+      console.log(err);
+      return res.render('register');
+    }
+    passport.authenticate('local')(req, res, function (){
+      res.redirect('/campgrounds');
+    });
+  });
+});
+
 
 app.listen(3000, function () {
   console.log('App has started on port 3000.');

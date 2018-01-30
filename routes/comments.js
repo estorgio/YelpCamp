6ba385e44887b10fed1ad22e05aa666e375592d3
estorgio <fortunato.estorgio@gmail.com>
2018-01-30 18:28:02 +0800

    Comment Edit and Update

--- routes/comments.js
+++ routes/comments.js
@@ -46,6 +46,28 @@ router.post('/', isLoggedIn, function (req, res) {
   })
 });
 
+// COMMENT EDIT ROUTE
+router.get('/:comment_id/edit', function (req, res) {
+  Comment.findById(req.params.comment_id, function (err, foundComment) {
+    if (err) {
+      console.log(err);
+      return res.redirect('back');
+    }
+    res.render('comments/edit', {campground_id: req.params.id, comment: foundComment});
+  });
+});
+
+// COMMENT UPDATE ROUTE
+router.put('/:comment_id', function (req, res) {
+  Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function (err, updatedComment) {
+    if (err) {
+      console.log(err);
+      return res.redirect('back');
+    }
+    res.redirect('/campgrounds/' + req.params.id);
+  });
+});
+
 // middleware
 function isLoggedIn(req, res, next) {
   if (req.isAuthenticated()) {
